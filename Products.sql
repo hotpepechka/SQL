70664fcf8185f99ffe43c8acb7ddfc29ce442a1c
hotpepechka <90408957+hotpepechka@users.noreply.github.com>
2023-11-14 12:22:50 +0500

    Add files via upload

---
+++ Products.sql
@@ -0,0 +1,4 @@
+SELECT Products.ProductName, Categories.CategoryName
+FROM Products
+LEFT JOIN ProductCategory ON Products.ProductID = ProductCategory.ProductID
+LEFT JOIN Categories ON ProductCategory.CategoryID = Categories.CategoryID;
